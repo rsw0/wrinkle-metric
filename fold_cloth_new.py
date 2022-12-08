5f1cd990cda10393cbd82c879bd36c319c17f173
Dongx1aoYang <119555079+Dongx1aoYang@users.noreply.github.com>
2022-12-07 22:19:52 -0500

    Update fold_cloth_new.py

--- fold_cloth_new.py
+++ fold_cloth_new.py
@@ -9,10 +9,10 @@ Output figures to a folder.
 
 # ----------- PARAMETERS -----------
 NOC = 60 # NUMBER OF CUT
-N = 1 # NUMBER OF RANDOM SAMPLES
+N = 1 # NUMBER OF RANDOM SAMPLES TO GENERATE
 X, Y = 0, 0 # PICK POSITION
 dx, dy = 1.5, 1.5 # DISTANCE TO PLACE
-ALPHA = 0.01 # threshold
+ALPHA = 0.010 # threshold
 # ----------------------------------
 
 dz = np.random.randint(low = 10, high = 100, size = N) / 100 # randomize pick-up height
@@ -151,7 +151,7 @@ for k in range(N):
         Simulate the folding process by rendering an animation.
         The hook moves from the initial position (frame 5) to (dx/2, dy/2, dz) on mid_frame through a linear path,
         then moves to (dx, dy, 0) on end_frame through a linear path.
-        The hook releases the cloth at the end.
+        The hook releases the cloth at the end (5 frames after placing).
         Return the scene object.
         '''
         
@@ -206,13 +206,19 @@ for k in range(N):
     # read the evaluated (deformed) mesh data into the bmesh object:
     bm.from_object(cloth , depgraph )
     sum_of_z = 0
+    highest_z = 0
     # iterate the bmesh verts:
     for i, v in enumerate(bm.verts):
         v.co[2] += 0.05 # calibrate thickness
-        sum_of_z += (v.co[2] - ALPHA)
+        z_val = max((v.co[2] - ALPHA), 0)
+        if(z_val > highest_z):
+            highest_z = z_val
+        sum_of_z += z_val
+        #sum_of_z += max((v.co[2] - ALPHA), 0)
         #print("frame: 50, vert: {}, location: {}".format(i, v.co))
 
     print("The sum of z-value of all vertices is %.3f." %sum_of_z)
+    print("The highest value of z is %.3f"%highest_z)
     sumz_values.append(sum_of_z)
 
     for area in bpy.context.screen.areas:
@@ -226,10 +232,10 @@ for k in range(N):
             area.spaces.active.region_3d.update() 
 
     # add camera
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 6), rotation=(0, -0, 0), scale=(1, 1, 1))
+    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 8), rotation=(0, -0, 0), scale=(1, 1, 1))
     scn.camera = bpy.context.object
 
     # render and output images as jpeg files
     bpy.context.scene.render.image_settings.file_format='JPEG'
-    bpy.context.scene.render.filepath = "/Users/donge/Documents/CU/6998 Robot Learning/blender/folded_fig/%d_dz_%.2f_frame_%d.jpg"%(k, dz[k], END_FRAME[k]) # YOUR OWN PATH
+    bpy.context.scene.render.filepath = "C:/Users/dongx/Desktop/folded_imgs/%d_dz_%.2f_frame_%d.jpg"%(k, dz[k], END_FRAME[k]) # YOUR OWN PATH
     bpy.ops.render.render(write_still = True, use_viewport = True)
